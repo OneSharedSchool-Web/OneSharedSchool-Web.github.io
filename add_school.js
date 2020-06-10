3c93195392c3e68c0f0114039f6a287e236bb0a8
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2020-06-10 09:52:45 -0700

    Adding admin portal for adding schools

---
+++ add_school.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+
+    firebase.database()
+        .ref("SchoolCodes")
+        .once('value')
+        .then((snapshot) => { 
+            console.log("snap " + snapshot.value)
+            snapshot.forEach(function(snap) {
+
+                console.log("inner: " + snap.value()) 
+            })
+            
+        });
+
+
+    $("#add_school_button_submit").click(function (e) {
+        e.preventDefault();
+        console.log("submit button clicked")
+
+        var schoolName = $("#schoolInput").val();
+
+        console.log(schoolName);
+    });
+
+
+
+});
