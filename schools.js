9b189fd47ff9642b5d123f400315b2bc71d858d5
SuperByakugan <43621807+SuperByakugan@users.noreply.github.com>
2020-06-12 00:37:16 -0700

    Add files via upload

--- schools.js
+++ schools.js
@@ -1,5 +1,5 @@
 window.onload = update();
-
+update();
 function update()
 {
 	var ref = firebase.database().ref('Schools');
@@ -27,6 +27,7 @@ function gotData(data)
 			var url = articleObj[k].imageUri;
 			var RM = articleObj[k].raisedMoney;
 			var TM = articleObj[k].totalMoney;
+			var fundLink = articleObj[k].fundLink;
 			
 			var schoolCard = document.createElement('div');
 			schoolCard.className = "schoolCard";
@@ -47,10 +48,41 @@ function gotData(data)
 			schoolMoney.className = "schoolMoney";
 			schoolMoney.innerHTML = "$" + RM + " / " + "$" + TM;
 			
+			var schoolLink = document.createElement('a');
+			schoolLink.href =  fundLink;
+			schoolLink.innerHTML = "Click Here for Go Fund Me"
+			
 			schoolInfo.appendChild(schoolTitle);
 			schoolInfo.appendChild(schoolDescription);
 			schoolInfo.appendChild(schoolMoney);
+			schoolInfo.appendChild(schoolLink);
 			schoolCard.appendChild(schoolInfo);
+			/*schoolCard.onclick = function()
+			{
+				var myNode = document.getElementById("content");
+				while (myNode.firstChild) 
+				{
+					myNode.removeChild(myNode.lastChild);
+				}
+				document.getElementById("blackborder").style.display = "flex";
+				var obj = document.getElementById("content");
+				
+				var schoolTitle = document.createElement('span');
+				var schoolDescription = document.createElement('p');
+				var schoolMoney = document.createElement('span');
+				
+				schoolTitle.id = "schoolTitleInside";
+				schoolDescription.id = "schoolDescriptionInside";
+				schoolMoney.id = "schoolMoneyInside";
+				schoolMoney.innerHTML = "$" + RM + " / " + "$" + TM;
+				
+				schoolTitle.innerHTML = name;
+				schoolDescription.innerHTML = description;
+				
+				obj.appendChild(schoolTitle);
+				obj.appendChild(schoolDescription);
+				obj.appendChild(schoolMoney);
+			}*/
 			document.getElementById("schoolContainer").appendChild(schoolCard);
 		}
 	}
@@ -62,4 +94,9 @@ function gotData(data)
 function errData(err)
 {
 	console.log(err);
+}
+
+function hide()
+{
+	document.getElementById("blackborder").style.display = "none";
 }
