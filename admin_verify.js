0359a0d544ffda1477d6d437d843bd376ac58a12
sreeharirammohan@gmail.com <wizmathhelp@gmail.com>
2020-06-11 22:21:13 -0700

    updates

--- admin_verify.js
+++ admin_verify.js
@@ -1,18 +1,48 @@
 $(document).ready(function () {
-    // $("#proposed_school_list").append("<div class=\"shadow\"><h6>Adding this card</h6></div>")
 
     var ref = firebase.database().ref('ProposedSchools');
 
+    // var schoolName = "test high school";
+    // var adminFirstName = "marian";
+    // var adminLastName = "webster";
+    // var schoolDescription = "description of some random school";
+    // var schoolImageLink = "https://www.brown.edu/sites/g/files/dprerj316/files/styles/square_sml/public/2019-04/01_About.jpg";
+    // var driversLicense = "https://s.hdnux.com/photos/70/31/77/14786535/8/920x920.jpg";
+    // var schoolID = "https://www.idwholesaler.com/learning-center/wp-content/uploads/2018/03/student-school-id-card-1.png";
+    // var otherVerificationInformation = "random other verification data";
+
+    // const card_to_inject = "<div class=\"shadow p-5\"><h4>" + schoolName + "</h4><div class=\"d-flex flex-row justify-content-center align-items-center p-3\"><div class=\"px-2\"><p>Admin:" + adminFirstName + " " + adminLastName + "</p><p>Description:" + schoolDescription + "</p><p>Items Needed: </p><ul><li>Hair</li><li>Clothing</li></ul></div><div class=\"px-2\"><img id=\"school_image\" src=\"" + schoolImageLink + "\"alt=\"School Image\"></div></div><h4>Admin Verification Information</h4><div class=\"d-flex flex-row justify-content-center align-items-center p-3\"><div class=\"p-3\"><h6>User Drivers License</h6><img id=\"drivers_license\" src=\"" + driversLicense + "\" alt=\"\"></div><div class=\"p-3\"><h6>School ID</h6><img id=\"school_id\"src=\"" + schoolID + "\"alt=\"\"></div></div><p>Other Verification Information:" + otherVerificationInformation + "</p><div class=\"d-flex flex-row justify-content-around align-items-center\" id=\"INSERT_USER_UID_HERE\"><div class=\"mr-3\"><p>approve</p><svg class=\"bi bi-check2\" width=\"5vw\" height=\"5vw\" viewBox=\"0 0 16 16\" fill=\"currentColor\"xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\"d=\"M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z\" /></svg></div><div><p>reject</p><svg class=\"bi bi-x\" width=\"5vw\" height=\"5vw\" viewBox=\"0 0 16 16\" fill=\"currentColor\"xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\"d=\"M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z\" /><path fill-rule=\"evenodd\"d=\"M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z\" /></svg></div></div></div>"
+    // $("#proposed_school_list").append(card_to_inject)
+
     ref.once("value", (data) => {
         var object = data.val();
-        
+
+        console.log("entire object ===================");
+        console.log(object);
+
         var keys = Object.keys(object);
+        console.log("keys", keys)
 
-        var content = data[keys[0]];
+        for (var i = 0; i < keys.length; i++) {
+            var current_key = keys[i];
+            var content = object[current_key];
+            console.log("content", content)
+            console.log(content)
 
-        console.log(content)
+            var schoolName = "content[\"adminFirstName\"]";
+            var adminFirstName = content["adminFirstName"];
+            var adminLastName = content["adminLastName"];
+            var schoolDescription = content["description"];
+            var schoolImageLink = content["imageURI"];
+            var driversLicense = content["adminFirstName"];
+            var schoolID = content["adminFirstName"];
+            var otherVerificationInformation = content["otherVerificationInfo"];
+            var driversLicense = "https://s.hdnux.com/photos/70/31/77/14786535/8/920x920.jpg";
+            var schoolID = "https://www.idwholesaler.com/learning-center/wp-content/uploads/2018/03/student-school-id-card-1.png";
+            const card_to_inject = "<div class=\"shadow p-5\"><h4>" + schoolName + "</h4><div class=\"d-flex flex-row justify-content-center align-items-center p-3\"><div class=\"px-2\"><p>Admin:" + adminFirstName + " " + adminLastName + "</p><p>Description:" + schoolDescription + "</p><p>Items Needed: </p><ul><li>Hair</li><li>Clothing</li></ul></div><div class=\"px-2\"><img id=\"school_image\" src=\"" + schoolImageLink + "\"alt=\"School Image\"></div></div><h4>Admin Verification Information</h4><div class=\"d-flex flex-row justify-content-center align-items-center p-3\"><div class=\"p-3\"><h6>User Drivers License</h6><img id=\"drivers_license\" src=\"" + driversLicense + "\" alt=\"\"></div><div class=\"p-3\"><h6>School ID</h6><img id=\"school_id\"src=\"" + schoolID + "\"alt=\"\"></div></div><p>Other Verification Information:" + otherVerificationInformation + "</p><div class=\"d-flex flex-row justify-content-around align-items-center\" id=\"INSERT_USER_UID_HERE\"><div class=\"mr-3\"><p>approve</p><svg class=\"bi bi-check2\" width=\"5vw\" height=\"5vw\" viewBox=\"0 0 16 16\" fill=\"currentColor\"xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\"d=\"M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z\" /></svg></div><div><p>reject</p><svg class=\"bi bi-x\" width=\"5vw\" height=\"5vw\" viewBox=\"0 0 16 16\" fill=\"currentColor\"xmlns=\"http://www.w3.org/2000/svg\"><path fill-rule=\"evenodd\"d=\"M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z\" /><path fill-rule=\"evenodd\"d=\"M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z\" /></svg></div></div></div>"
+            $("#proposed_school_list").append(card_to_inject)
+        }
 
-        console.log("object keys: ", keys);
     }, null)
 
 });
